f2f494eaaed40c29ae15d22c1c4e0afa4533bdf7
Júlio Cesar Santos <phalkao@gmail.com>
2022-07-19 10:42:30 -0300

    Configuração do dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="col-xl-12">
+                        <div class="card">
+                            <div class="card-body">
+                                @php
+                                    echo env('MAIL_FROM_ADDRESS');
+                                @endphp
+                                <div class="row">
+                                    <div id="presenca" class="col-xl-4">
+                                        <h5 style="text-align: center;">Canal de Presença</h5>
+                                        <div id="user-presenca" class="alert alert-primary">
+                                            <p>Usuários Conectados</p>
+                                        </div>
+                                    </div>
+                                    <div id="privado" class="col-xl-4">
+                                        <h5 style="text-align: center;">Canal Privado</h5>
+                                    </div>
+                                    <div id="publico" class="col-xl-4">
+                                        <h5 style="text-align: center;"> Canal Publico</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
